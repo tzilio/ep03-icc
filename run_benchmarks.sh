2a6079c30f8b6de99b386a3fe34b738a55bb89a2
Thiago Zilio <tzilioassuncao@gmail.com>
2025-06-29 15:11:47 -0300

    oie

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,60 +1,65 @@
 #!/bin/bash
 
 # script de automatizacao de testes utilizado na maquina dinf
-CPU=3
-GRAUS=(10 1000)
+CORE=3
+DEGREES=(10 1000)
 
-PROGRAMAS=("ajustePol_v1" "ajustePol_v2") 
+VERSIONS=("ajustePol_v1" "ajustePol_v2") 
 
-N1=(64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000)
-N2=(64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000)
-make all
+BASE_N1=(64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000 1000000 10000000 100000000)
+BASE_N2=(64 128 200 256 512 600 800 1024 2000 3000 4096 6000 7000 10000 50000 100000)
 
-# iterando pelos programas (v1 e v2)
-for PROGRAMA in "${PROGRAMAS[@]}"; do
-    OUTPUT_TSL_FILE="results_${PROGRAMA}_tsl.csv"
-    OUTPUT_TEG_FILE="results_${PROGRAMA}_teg.csv"
+echo "gerando executaveis"
+make likwid
+make gera_entrada
 
-    # setando o csv dos resultados
-    echo "GRAU,PONTOS,timeSL,L3MISSRATIO,ENERGY,FLOPS_DP,FLOPS_AVX_DP" > "$OUTPUT_TSL_FILE"
-    echo "GRAU,PONTOS,timeEG,L3MISSRATIO,ENERGY,FLOPS_DP,FLOPS_AVX_DP" > "$OUTPUT_TEG_FILE"
 
-    # funcao que executa os comandos e da grep no que precisa
-    calcula(){
-        local grau=$1
-        local pontos=$2
-        local SAIL3="temp_l3.out"
-        local SAIENERGY="temp_energy.out"
-        local SAIFLOPS="temp_flops.out"
+for PROGRAM in "${VERSIONS[@]}"; do
+    OUTPUT_SL_FILE="results_${PROGRAM}_sl.csv"
+    OUTPUT_EG_FILE="results_${PROGRAM}_eg.csv"
 
-        # gera a entrada com a metrica requisitada, da grep e coloca num arquivo .out
-        echo "comecando a gerar proximo programa da fila..."
-        ./gera_entrada "$pontos" "$grau" | likwid-perfctr -C $CPU -g FLOPS_DP -m ./"$PROGRAMA" > "$SAIFLOPS"
-        local dp_tsl=$(cat "$SAIFLOPS" | grep "DP" | awk '{print $5}' | sed -n '2p')
-        local dp_teg=$(cat "$SAIFLOPS" | grep "DP" | awk '{print $5}' | sed -n '5p')
-        local AVX_dp_tsl=$(cat "$SAIFLOPS" | grep "DP" | awk '{print $6}' | sed -n '3p')
-        local AVX_dp_teg=$(cat "$SAIFLOPS" | grep "DP" | awk '{print $6}' | sed -n '6p')
-        ./gera_entrada "$pontos" "$grau" | likwid-perfctr -C $CPU -g L3CACHE -m ./"$PROGRAMA" > "$SAIL3"
-        local l3_miss_ratio_tsl=$(grep "L3 miss ratio" "$SAIL3" | awk '{print $6}' | sed -n '1p')
-        local l3_miss_ratio_teg=$(grep "L3 miss ratio" "$SAIL3" | awk '{print $6}' | sed -n '2p')
-        ./gera_entrada "$pontos" "$grau" | likwid-perfctr -C $CPU -g ENERGY -m ./"$PROGRAMA" > "$SAIENERGY"
-        local energy_tsl=$(grep "Energy" "$SAIENERGY" | sed -n '1p' | awk '{print $5}')
-        local energy_teg=$(grep "Energy" "$SAIENERGY" | sed -n '5p' | awk '{print $5}')
+    # CSV headers
+    echo 'DEGREE,POINTS,TSL,L3MISSRATIO,ENERGY,FLOPS_DP,FLOPS_AVX_DP' > "$OUTPUT_SL_FILE"
+    echo 'DEGREE,POINTS,TEG,L3MISSRATIO,ENERGY,FLOPS_DP,FLOPS_AVX_DP' > "$OUTPUT_EG_FILE"
 
-        local tsl teg
-        read -r _ tsl teg <<< $(grep "^$pontos" "$SAIL3")
-        echo "$grau,$pontos,$tsl,$l3_miss_ratio_tsl,$energy_tsl,$dp_tsl,$AVX_dp_tsl" >> "$OUTPUT_TSL_FILE"
-        echo "$grau,$pontos,$teg,$l3_miss_ratio_teg,$energy_teg,$dp_teg,$AVX_dp_teg" >> "$OUTPUT_TEG_FILE"
-    } 
-    # "main" do script
-    for GRAU in "${GRAUS[@]}"; do
-        if [ "$GRAU" -eq 10 ]; then
-            PONTOS=("${N1[@]}")
+    metrics() {
+        local degree=$1
+        local points=$2
+        local OUT_L3='temp_l3.out'
+        local OUT_ENERGY='temp_energy.out'
+        local OUT_FLOPS='temp_flops.out'
+
+        echo 'starting next program in queue...'
+        ./gera_entrada "$points" "$degree" | likwid-perfctr -C $CORE -g FLOPS_DP -m ./"$PROGRAM" > "$OUT_FLOPS"
+        local dp_sl=$(grep 'DP' "$OUT_FLOPS" | awk '{print $5}' | sed -n '2p')
+        local dp_eg=$(grep 'DP' "$OUT_FLOPS" | awk '{print $5}' | sed -n '5p')
+        local avx_dp_sl=$(grep 'DP' "$OUT_FLOPS" | awk '{print $6}' | sed -n '3p')
+        local avx_dp_eg=$(grep 'DP' "$OUT_FLOPS" | awk '{print $6}' | sed -n '6p')
+
+        ./gera_entrada "$points" "$degree" | likwid-perfctr -C $CORE -g L3CACHE -m ./"$PROGRAM" > "$OUT_L3"
+        local l3_miss_ratio_sl=$(grep 'L3 miss ratio' "$OUT_L3" | awk '{print $6}' | sed -n '1p')
+        local l3_miss_ratio_eg=$(grep 'L3 miss ratio' "$OUT_L3" | awk '{print $6}' | sed -n '2p')
+
+        ./gera_entrada "$points" "$degree" | likwid-perfctr -C $CORE -g ENERGY -m ./"$PROGRAM" > "$OUT_ENERGY"
+        local energy_sl=$(grep 'Energy' "$OUT_ENERGY" | sed -n '1p' | awk '{print $5}')
+        local energy_eg=$(grep 'Energy' "$OUT_ENERGY" | sed -n '5p' | awk '{print $5}')
+
+        local time_sl time_eg
+        read -r _ time_sl time_eg <<< "$(grep "^$points" "$OUT_L3")"
+
+        echo "${degree},${points},${time_sl},${l3_miss_ratio_sl},${energy_sl},${dp_sl},${avx_dp_sl}" >> "$OUTPUT_SL_FILE"
+        echo "${degree},${points},${time_eg},${l3_miss_ratio_eg},${energy_eg},${dp_eg},${avx_dp_eg}" >> "$OUTPUT_EG_FILE"
+    }
+
+    # main loop
+    for DEGREE in "${DEGREES[@]}"; do
+        if [ "$DEGREE" -eq 10 ]; then
+            POINTS_LIST=("${POINTS_N1[@]}")
         else
-            PONTOS=("${N2[@]}")
+            POINTS_LIST=("${POINTS_N2[@]}")
         fi
-        for PONTO in "${PONTOS[@]}"; do
-            calcula "$GRAU" "$PONTO"
+        for POINTS in "${POINTS_LIST[@]}"; do
+            metrics "$DEGREE" "$POINTS"
         done
     done
 done
